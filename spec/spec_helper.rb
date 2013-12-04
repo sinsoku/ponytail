0d927b8ddbb9beb76f0b3f7305d2993d2616e866
sinsoku <sinsoku.listy@gmail.com>
2013-12-05 06:49:08 +0900

    add coveralls setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+if ENV["TRAVIS"]
+  require 'coveralls'
+  Coveralls.wear! do
+    add_group 'Lib', 'lib'
+
+    add_filter 'spec'
+    add_filter 'vendor'
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_record'
 require 'rails'
