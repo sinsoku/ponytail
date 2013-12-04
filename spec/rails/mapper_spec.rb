0608d5415c89041c50caa8b7b1bc13896f77d4b1
sinsoku <sinsoku.listy@gmail.com>
2013-12-05 08:00:32 +0900

    add spec for mount_ponytail

---
+++ spec/rails/mapper_spec.rb
@@ -0,0 +1,17 @@
+module ActionDispatch::Routing
+  describe Mapper do
+    describe "#mount_ponytail" do
+      let(:mapper) do
+        dummy = Class.new { def resources_path_names; end }
+        Mapper.new(dummy.new)
+      end
+
+      it "should receive resources" do
+        mapper.should_receive(:resources) { |*args, &block| block.call }
+        mapper.should_receive(:collection) { |*args, &block| block.call }
+        mapper.should_receive(:post).twice
+        mapper.mount_ponytail
+      end
+    end
+  end
+end
