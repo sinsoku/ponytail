50370c202277954b07b4712725ab32c46ccf6a98
sinsoku <sinsoku.listy@gmail.com>
2013-12-12 02:28:35 +0900

    refactor by object literal

--- app/assets/javascripts/ponytail/create_table.js
+++ app/assets/javascripts/ponytail/create_table.js
@@ -1,13 +1,13 @@
 function CreateTable() {
   this.tableName = "";
 }
-
-CreateTable.prototype.setTableName = function(tableName) {
-  this.tableName = tableName;
-};
-
-CreateTable.prototype.toString = function() {
-  return ["create_table :" + this.tableName + " do |t|",
-         "  t.timestamps",
-         "end"].join("\n");
+CreateTable.prototype = {
+  setTableName: function(tableName) {
+    this.tableName = tableName;
+  },
+  toString: function() {
+    return ["create_table :" + this.tableName + " do |t|",
+           "  t.timestamps",
+           "end"].join("\n");
+  }
 };
