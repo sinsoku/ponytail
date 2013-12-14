23fbaa09068212d323fcbe8995bfc20a3bd67842
sinsoku <sinsoku.listy@gmail.com>
2013-12-14 17:20:27 +0900

    inline method in application.js

--- app/assets/javascripts/ponytail/application.js
+++ app/assets/javascripts/ponytail/application.js
@@ -1,11 +1,1 @@
 //= require_tree .
-
-function setupMigrations() {
-  toggleMigrationRawContent();
-  closeNotice();
-}
-
-function setupNewMigration() {
-  var elem = document.querySelectorAll(".pt_new")[0];
-  new NewMigrationView(elem).init();
-}
