f10dda6007e7fedc181e29c94be9a364b6aa044a
sinsoku <sinsoku.listy@gmail.com>
2013-12-14 17:16:15 +0900

    when click "New Table" button, append TableView

--- app/assets/javascripts/ponytail/table_view.js
+++ app/assets/javascripts/ponytail/table_view.js
@@ -5,9 +5,27 @@ function TableView(option) {
 TableView.prototype = {
   init: function(option) {
     this.element = option.element;
+    if (this.element === undefined) {
+      this.element = document.createElement("div");
+      this.element.setAttribute("class", "pt_table");
+      this.element.innerHTML = '<div class="pt_table_name"><input type="text" /></div><div class="pt_columns"></div>';
+    }
+    this.columnElements = document.querySelectorAll(".pt_column");
     this.callbacks = [];
+    this.command = null;
   },
   addChangeListener: function(callback) {
     this.callbacks.push(callback);
+  },
+  toCommands: function() {
+    // TODO: fake it
+    if (this.command !== null) {
+      return [this.command];
+    } else {
+      return [];
+    }
+  },
+  toElement: function() {
+    return this.element;
   }
 };
