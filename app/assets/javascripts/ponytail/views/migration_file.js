52a7106f79eb0bad2669e74780873eeccf59efa2
sinsoku <sinsoku.listy@gmail.com>
2013-12-18 05:03:01 +0900

    add migration file view

--- app/assets/javascripts/ponytail/views/migration_file.js
+++ app/assets/javascripts/ponytail/views/migration_file.js
@@ -1,8 +1,20 @@
 Ponytail.Views.MigrationFileView = Backbone.View.extend({
   events: {
+    "click .pt_edit_checkbox input": "toggleRawContent",
+    "click .pt_submit": function() { this.setRawContentEnabled(true); },
   },
   initialize: function(options) {
   },
   render: function() {
+  },
+  toggleRawContent: function() {
+    this.setRawContentEnabled($(".pt_raw_content textarea").attr("disabled"));
+  },
+  setRawContentEnabled: function(enabled) {
+    if (enabled) {
+      $(".pt_raw_content textarea").removeAttr("disabled");
+    } else {
+      $(".pt_raw_content textarea").attr("disabled", "disabled");
+    }
   },
 });
--- app/assets/javascripts/ponytail/views/new_migrations.js
+++ app/assets/javascripts/ponytail/views/new_migrations.js
@@ -1,2 +1,6 @@
 Ponytail.Views.NewMigrationView = Backbone.View.extend({
+  render: function() {
+    var migrationFile = new Ponytail.Models.MigrationFile();
+    new Ponytail.Views.MigrationFileView({el: $(".migration_file_view"), model: migrationFile}).render();
+  }
 });
