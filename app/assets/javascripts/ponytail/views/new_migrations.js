919b2f00fb1cf1c94b625e137afb8b748f1c6e9f
sinsoku <sinsoku.listy@gmail.com>
2013-12-18 05:39:55 +0900

    impl appendNewTableLink

--- app/assets/javascripts/ponytail/views/new_migrations.js
+++ app/assets/javascripts/ponytail/views/new_migrations.js
@@ -1,6 +1,18 @@
 Ponytail.Views.NewMigrationView = Backbone.View.extend({
-  render: function() {
+  events: {
+    "click .pt_new_table a": "appendTableView"
+  },
+  initialize: function() {
+    $(".pt_table").each(function() {
+      new Ponytail.Views.TableView({el: $(this)});
+    });
     var migrationFile = new Ponytail.Models.MigrationFile();
-    new Ponytail.Views.MigrationFileView({el: $(".migration_file_view"), model: migrationFile}).render();
+    new Ponytail.Views.MigrationFileView({el: $(".migration_file_view"), model: migrationFile});
+  },
+  appendTableView: function() {
+    var m = new Ponytail.Models.Table();
+    var v = new Ponytail.Views.TableView({model: m});
+    $(".pt_new_table").before(v.render().el);
+    return false;
   }
 });
--- app/assets/javascripts/ponytail/views/table.js
+++ app/assets/javascripts/ponytail/views/table.js
@@ -1,8 +1,11 @@
 Ponytail.Views.TableView = Backbone.View.extend({
+  className: "pt_table",
   events: {
   },
   initialize: function(options) {
   },
   render: function() {
+    $(this.el).html(_.template($("#table_view_template").html(), this.model.attributes));
+    return this;
   },
 });
