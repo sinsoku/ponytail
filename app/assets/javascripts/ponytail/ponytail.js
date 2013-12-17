24205f3c54db064902abf03f40bc0aa1d572f88a
sinsoku <sinsoku.listy@gmail.com>
2013-12-18 03:43:36 +0900

    refactor by jQuery

--- app/assets/javascripts/ponytail/ponytail.js
+++ app/assets/javascripts/ponytail/ponytail.js
@@ -4,3 +4,12 @@ window.Ponytail = {
   Views: {},
   Routers: {},
 };
+
+function setupIndex() {
+  $(".pt_close_notice").click(function() {
+    $(".pt_notice").hide();
+  });
+  $(".pt_name, .pt_filename").click(function() {
+    $(".pt_raw_content", $(this).parent().parent()).toggle();
+  });
+}
