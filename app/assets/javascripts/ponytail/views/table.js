52a601f56b86ef69fa132c77f916b49f73d92c5a
sinsoku <sinsoku.listy@gmail.com>
2013-12-19 02:27:26 +0900

    bind tableName render

--- app/assets/javascripts/ponytail/views/table.js
+++ app/assets/javascripts/ponytail/views/table.js
@@ -2,12 +2,19 @@ Ponytail.Views.TableView = Backbone.View.extend({
   className: "pt_table",
   events: {
     "click .pt_table_name span": "toggleTableName",
-    "blur .pt_table_name input": function() { this.updateTableName(); this.toggleTableName(); },
+    "keyup .pt_table_name input": function(e) { this.model.set({name: e.target.value}); },
+    "blur .pt_table_name input": "toggleTableName",
   },
   initialize: function(options) {
+    _.bindAll(this, "render");
+    this.model.bind("change", this.render);
   },
   render: function() {
-    $(this.el).html(_.template($("#table_view_template").html(), this.model.attributes));
+    if (this.el.parentNode === null) {
+      $(this.el).html(_.template($("#table_view_template").html(), this.model.attributes));
+    } else {
+      $(this.el).find(".pt_table_name span").text(this.model.get("name"));
+    }
     return this;
   },
   updateTableName: function() {
