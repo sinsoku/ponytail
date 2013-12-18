6d52a43c49487ab2915db5748e17f2fdad186d10
sinsoku <sinsoku.listy@gmail.com>
2013-12-18 19:44:30 +0900

    delete old js specs

--- spec/javascripts/ponytail.bak/models/migration_file_spec.js
+++
@@ -1,9 +0,0 @@
-describe("MigrationFile", function() {
-  describe("#setClassName", function() {
-    it("should be able to set className", function() {
-      var file = new MigrationFile();
-      file.setClassName("CreateUsers");
-      expect(file.className).toBe("CreateUsers");
-    });
-  });
-});
