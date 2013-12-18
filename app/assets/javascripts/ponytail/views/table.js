e394b1ec4ac71ff717727c94158145e6ff4b0be8
sinsoku <sinsoku.listy@gmail.com>
2013-12-18 17:00:48 +0900

    edit table name

--- app/assets/javascripts/ponytail/views/table.js
+++ app/assets/javascripts/ponytail/views/table.js
@@ -1,11 +1,21 @@
 Ponytail.Views.TableView = Backbone.View.extend({
   className: "pt_table",
   events: {
+    "click .pt_table_name span": "toggleTableName",
+    "blur .pt_table_name input": function() { this.updateTableName(); this.toggleTableName(); },
   },
   initialize: function(options) {
   },
   render: function() {
     $(this.el).html(_.template($("#table_view_template").html(), this.model.attributes));
     return this;
+  },
+  updateTableName: function() {
+    var v = $(this.el).find(".pt_table_name input")[0].value;
+    $(this.el).find(".pt_table_name span").text(v);
+  },
+  toggleTableName: function() {
+    $(this.el).find(".pt_table_name span").toggle();
+    $(this.el).find(".pt_table_name input").toggle();
   },
 });
