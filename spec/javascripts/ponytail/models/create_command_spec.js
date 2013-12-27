39acf4142d85be989ba98abf89e9099e86a71d89
sinsoku <sinsoku.listy@gmail.com>
2013-12-27 23:07:54 +0900

    delete console.log

--- spec/javascripts/ponytail/models/create_command_spec.js
+++ spec/javascripts/ponytail/models/create_command_spec.js
@@ -2,7 +2,6 @@ describe("CreateTableCommand", function() {
   describe("#toString", function() {
     it("it should be able to get string", function() {
       var column = buildModel({type: 'string', name: 'name'});
-      console.log(column.get("type"));
       var command = new Ponytail.Models.CreateTableCommand('users', [column]);
       var expected = [
         "create_table :users do |t|",
