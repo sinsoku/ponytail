c3aeab29da444a933ce154bb028e5749b5218e5a
sinsoku <sinsoku.listy@gmail.com>
2014-07-17 19:25:47 +0900

    Update routing for new design

--- lib/ponytail/routing/mapper.rb
+++ lib/ponytail/routing/mapper.rb
@@ -3,8 +3,7 @@ module ActionDispatch::Routing
     def mount_ponytail
       namespace :ponytail do
         resources :migrations, only: [:index, :new, :create, :destroy]
-        resources :tables, only: [:index, :show], path: :schema, as: :schema
-        resource :schema, only: :update, controller: :schema
+        resources :schema, only: [:index, :show, :update]
       end
     end
   end
